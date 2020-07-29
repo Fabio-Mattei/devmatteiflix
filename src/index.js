118f73244c8f09405d139010c376de5a488d52d7
Fabio <emaildomattei@gmail.com>
2020-07-29 00:46:20 -0300

    Commit parcial aula 2

--- src/index.js
+++ src/index.js
@@ -1,11 +1,20 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+import Home from './Pages/Home';
+import { BrowserRouter, Switch, Route } from 'react-router-dom';
+import CadastroVideo from './Pages/Cadastro/Video'
+import CadastroCategoria from './Pages/Cadastro/Categoria';
 
+const Pagina404 = () => (<div>Página 404</div>)
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <BrowserRouter>
+    <Switch>
+      <Route path="/" component={Home} exact></Route>      
+      <Route path="/cadastro/video" component={CadastroVideo}></Route>
+      <Route path="/cadastro/categoria" component={CadastroCategoria}></Route>
+      <Route component={Pagina404}></Route>
+    </Switch>
+  </BrowserRouter>,
   document.getElementById('root')
 );
