f411994d2ff680c5e887b283207c303a69aadedf
Fabio <emaildomattei@gmail.com>
2020-08-02 07:56:57 -0300

    cadastro de vídeos

--- src/index.js
+++ src/index.js
@@ -1,20 +1,23 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import Home from './Pages/Home';
-import { BrowserRouter, Switch, Route } from 'react-router-dom';
-import CadastroVideo from './Pages/Cadastro/Video'
-import CadastroCategoria from './Pages/Cadastro/Categoria';
 
-const Pagina404 = () => (<div>Página 404</div>)
+import { BrowserRouter, Switch, Route } from 'react-router-dom';
+import Home from './pages/Home';
+import CadastroVideo from './pages/cadastro/Video';
+import CadastroCategoria from './pages/cadastro/Categoria';
+
+// Desafio master blaster na descrição
+const Pagina404 = () => (<div>Página 404</div>);
+
 ReactDOM.render(
   <BrowserRouter>
     <Switch>
-      <Route path="/" component={Home} exact></Route>      
-      <Route path="/cadastro/video" component={CadastroVideo}></Route>
-      <Route path="/cadastro/categoria" component={CadastroCategoria}></Route>
-      <Route component={Pagina404}></Route>
+      <Route path="/" component={Home} exact />
+      <Route path="/cadastro/video" component={CadastroVideo} />
+      <Route path="/cadastro/categoria" component={CadastroCategoria} />
+      <Route component={Pagina404} />
     </Switch>
   </BrowserRouter>,
-  document.getElementById('root')
+  document.getElementById('root'),
 );
